1a5c87ed079fe4985f71f31ebc915e1b031e8a3d
Abizer Nasir <abizern@abizern.org>
2013-08-01 15:05:32 +0100

    Initial commit

---
+++ TicketingHub-ios/TicketingHub_ios.h
@@ -0,0 +1,13 @@
+//
+//  TicketingHub_ios.h
+//  TicketingHub-ios
+//
+//  Created by Abizer Nasir on 01/08/2013.
+//  Copyright (c) 2013 TicketingHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TicketingHub_ios : NSObject
+
+@end
---
+++ TicketingHub-ios/TicketingHub_ios.m
@@ -0,0 +1,13 @@
+//
+//  TicketingHub_ios.m
+//  TicketingHub-ios
+//
+//  Created by Abizer Nasir on 01/08/2013.
+//  Copyright (c) 2013 TicketingHub. All rights reserved.
+//
+
+#import "TicketingHub_ios.h"
+
+@implementation TicketingHub_ios
+
+@end
