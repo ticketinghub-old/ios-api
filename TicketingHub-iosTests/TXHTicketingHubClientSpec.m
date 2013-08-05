1f92618ed1a7e5334d1a06dcdaf7e1094e0d2075
Abizer Nasir <abizern@abizern.org>
2013-08-05 19:00:26 +0100

    Refactor token tests to a separate spec file

--- TicketingHub-iosTests/TXHTicketingHubClientSpec.m
+++ TicketingHub-iosTests/TXHTicketingHubClientSpec.m
@@ -22,12 +22,10 @@
 // Expose non-public interface
 - (_TXHNetworkOAuthClient *)oauthClient;
 - (_TXHNetworkClient *)networkClient;
-- (NSString *)token;
-- (NSString *)refreshToken;
 
 @end
 
-SpecBegin(TXHTicketingHubClientSpec)
+SpecBegin(TXHTicketingHubClient)
 
 __block TXHTicketingHubClient *_client;
 
@@ -71,71 +69,5 @@ describe(@"sharedClient", ^{
     });
 
 });
-
-describe(@"configuring the shared client with OAuth parameters", ^{
-    // These are dummy parameters, the response is stubbed.
-    NSString * const _username = @"username";
-    NSString * const _password = @"password";
-    NSString * const _clientId = @"abcdefg";
-    NSString * const _clientSecret = @"hijklmn";
-
-    afterEach(^{
-        [OHHTTPStubs removeAllRequestHandlers];
-    });
-
-    context(@"with correct parameters", ^{
-
-        beforeEach(^{
-            NSDictionary *httpHeaders = @{@"Content-Type" : @"application/json"};
-
-            [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
-                // Only deal with the token endpoint
-                return [request.URL.lastPathComponent isEqualToString:@"token"];
-            } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
-                return [OHHTTPStubsResponse responseWithFile:@"tokenSuccess.json" statusCode:200 responseTime:0.0 headers:httpHeaders];
-            }];
-        });
-
-        it(@"stores the token and refresh token internally", ^AsyncBlock{
-            [_client configureWithUsername:_username password:_password clientId:_clientId clientSecret:_clientSecret success:^(NSURLRequest *request, NSHTTPURLResponse *response) {
-                expect([_client token]).to.equal(@"bd0fbf8ee4da7472c382c28e7f7b9977cd6768dcadd7b9328a84a5bd5e7e9b5e");
-                expect([_client refreshToken]).to.equal(@"0e6314d94b3eac772d571c8da04bdeb2d1cb3ace71487672bd54e83e968681a3");
-                done();
-
-            } error:^(NSHTTPURLResponse *response, NSError *error, id JSON) {
-                expect(@YES).will.beNil(); // We shouldn't call the error block in this case.
-                done();
-            }];
-
-        });
-    });
-
-    context(@"with incorrect parameters", ^{
-        beforeEach(^{
-            NSDictionary *httpHeaders = @{@"Content-Type" : @"application/json"};
-
-            [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
-                // Only deal with the token endpoint
-                return [request.URL.lastPathComponent isEqualToString:@"token"];
-            } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
-                return [OHHTTPStubsResponse responseWithFile:@"tokenFailure.json" statusCode:401 responseTime:0.0 headers:httpHeaders];
-            }];
-        });
-
-        it(@"calls the error block and doesn't set a token or refreshToken", ^AsyncBlock{
-            [_client configureWithUsername:_username password:_password clientId:_clientId clientSecret:_clientSecret success:^(NSURLRequest *request, NSHTTPURLResponse *response) {
-                expect(NO).to.beTruthy(); // We should not be calling the success block in this case
-                done();
-
-            } error:^(NSHTTPURLResponse *response, NSError *error, id JSON) {
-                expect([_client token]).to.beNil();
-                expect([_client refreshToken]).to.beNil();
-                done();
-            }];
-        });
-
-    });
-});
-
 
 SpecEnd
