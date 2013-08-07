d1c85a0867735c4e338844414ffb512192c163d8
Abizer Nasir <abizern@abizern.org>
2013-08-07 14:41:46 +0100

    Merge changes from master

--- NSDictionary+JCSKeyMapping/NSDictionary+JCSKeyMapping.h
+++ NSDictionary+JCSKeyMapping/NSDictionary+JCSKeyMapping.h
@@ -13,7 +13,7 @@
 /*! A category method that replaces the keys in the receiver with the new key provided in the keyMapping
  *
  *  If no mapping is provided for a key, the key is left unchanged.
- *  \param keymapping a dictionary which maps the existing key to the new key. If no mapping is provided for a key it is unchanged.
+ *  \param keyMapping a dictionary which maps the existing key to the new key. If no mapping is provided for a key it is unchanged.
  *  \param removeNulls If YES, then if the value for a key in the receiver is [NSNull null], then it is not in the returned dictionary.
  *  \returns A new NSDictionary with the keys changed according to the mapping, on optionally with [NSNull null] values removed.
  */
