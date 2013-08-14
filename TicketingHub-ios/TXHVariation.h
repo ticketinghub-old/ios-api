3088aae90b33ac48f597e3dcc341d08583db666d
Abizer Nasir <abizern@abizern.org>
2013-08-14 18:06:03 +0100

    Add TXHVariation files to the project and repository
    Just stubs at the moment.

---
+++ TicketingHub-ios/TXHVariation.h
@@ -0,0 +1,13 @@
+//
+//  TXHVariation.h
+//  TicketingHub-ios
+//
+//  Created by Abizer Nasir on 14/08/2013.
+//  Copyright (c) 2013 TicketingHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TXHVariation : NSObject
+
+@end
---
+++ TicketingHub-ios/TXHVariation.m
@@ -0,0 +1,13 @@
+//
+//  TXHVariation.m
+//  TicketingHub-ios
+//
+//  Created by Abizer Nasir on 14/08/2013.
+//  Copyright (c) 2013 TicketingHub. All rights reserved.
+//
+
+#import "TXHVariation.h"
+
+@implementation TXHVariation
+
+@end
