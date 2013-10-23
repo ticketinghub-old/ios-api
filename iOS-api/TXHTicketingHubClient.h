4de8789f6bf4802715fa1f9af143f460f090e8f2
Abizer Nasir <abizern@abizern.org>
2013-10-23 18:21:18 +0100

    Clear out old project files
    Create new client classes.
    
    Broadsword approach, but I'll be using a new project file with different
    names, so this is easier.

---
+++ iOS-api/TXHTicketingHubClient.h
@@ -0,0 +1,13 @@
+//
+//  TXHTicketingHubClient.h
+//  iOS-api
+//
+//  Created by Abizer Nasir on 23/10/2013.
+//  Copyright (c) 2013 TicketingHub. All rights reserved.
+//
+
+@import Foundation;
+
+@interface TXHTicketingHubClient : NSObject
+
+@end
---
+++ iOS-api/TXHTicketingHubClient.m
@@ -0,0 +1,13 @@
+//
+//  TXHTicketingHubClient.m
+//  iOS-api
+//
+//  Created by Abizer Nasir on 23/10/2013.
+//  Copyright (c) 2013 TicketingHub. All rights reserved.
+//
+
+#import "TXHTicketingHubClient.h"
+
+@implementation TXHTicketingHubClient
+
+@end
