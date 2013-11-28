5f3c561e53bc98ad9734ed89b6a3bbe95f5ac5ed
Abizer Nasir <abizern@abizern.org>
2013-11-28 18:58:29 +0000

    Add a top level header for the library

---
+++ iOS-api/iOS-api.h
@@ -0,0 +1,15 @@
+//
+//  iOS-api.h
+//  iOS-api
+//
+//  Created by Abizer Nasir on 28/11/2013.
+//  Copyright (c) 2013 TicketingHub. All rights reserved.
+//
+//  The main include header for the library
+
+#import "TXHTicketingHubClient.h"
+#import "TXHAPIError.h"
+
+#import "TXHProduct.h"
+#import "TXHSupplier.h"
+#import "TXHUser.h"
