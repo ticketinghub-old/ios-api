d458a04dc71242805d6d58454ebbb998d0b16a3b
Abizer Nasir <abizern@abizern.org>
2014-01-30 11:26:51 +0000

    Partial test setup

--- iOS-apiTests/ClientAvailabilitySpec.m
+++ iOS-apiTests/ClientAvailabilitySpec.m
@@ -10,6 +10,7 @@
 
 #import "Expecta.h"
 #import "Specta.h"
+#import "OHHTTPStubs.h"
 
 #import "TXHTicketingHubClient.h"
 
@@ -21,6 +22,7 @@ SpecBegin(ClientAvailability)
 
 __block TXHTicketingHubClient *_client;
 __block TXHProduct *_product;
+__block NSDictionary *_standardHTTPHeaders;
 
 before(^{
     _client = [[TXHTicketingHubClient alloc] initWithStoreURL:nil];
@@ -29,17 +31,37 @@ before(^{
                                  @"name": @"Product Name"};
     _product = [TXHProduct createWithDictionary:dictionary inManagedObjectContext:_client.managedObjectContext];
     [_client.managedObjectContext save:NULL];
+    _standardHTTPHeaders = @{@"Content-Type" : @"application/json"};
 });
 
 after(^{
+    _standardHTTPHeaders = nil;
     _product = nil;
     _client = nil;
 });
 
 describe(@"updating availabilities for a product", ^{
     context(@"with a new product and getting availabilities for a range", ^{
+        __block __weak id<OHHTTPStubsDescriptor> _availibilityStub;
+
         context(@"where there are availabilities", ^{
-            //
+            before(^{
+                _availibilityStub = [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+                    return [request.URL.lastPathComponent isEqualToString:@"availability"];
+                } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
+                    return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFileInBundle(@"AvailabilityDecember2013.json", nil) statusCode:200 headers:_standardHTTPHeaders];
+                }];
+
+                _availibilityStub.name = @"AvailabilityDecember2013.json";
+            });
+
+            after(^{
+                [OHHTTPStubs removeStub:_availibilityStub];
+            });
+
+            it(@"should create the basic availabilities", ^{
+                
+            });
         });
 
         context(@"where there are no availabilities", ^{
