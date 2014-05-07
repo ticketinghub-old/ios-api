737673b4a78cfb5555bea91d887ab4c65cffccd9
Bartek Hugo <bartekhugo@me.com>
2014-05-07 15:53:57 +0200

    Hiding Payment keys

--- iOS-api/Human/TXHPayment.h
+++ iOS-api/Human/TXHPayment.h
@@ -1,16 +1,5 @@
 #import "_TXHPayment.h"
 
-extern NSString * const kAmountKey;
-extern NSString * const kAuthorizationKey;
-extern NSString * const kAVSResultKey;
-extern NSString * const kCurrencyKey;
-extern NSString * const kPostalMatchKey;
-extern NSString * const kSecurityCodeResultKey;
-extern NSString * const kStreetMatchKey;
-extern NSString * const kTypeKey;
-extern NSString * const kAddressKey;
-extern NSString * const kCardKey;
-
 @interface TXHPayment : _TXHPayment {}
 
 + (instancetype)createWithDictionary:(NSDictionary *)dictionary inManagedObjectContext:(NSManagedObjectContext *)moc;
